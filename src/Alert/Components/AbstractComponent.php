d685a3119e18fc2a9741543c708b596d37e2e870
topsystem <admin@topsystem.cc>
2022-08-03 10:54:49 +0800

    :bug:fixbug

---
+++ src/Alert/Components/AbstractComponent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TopSystem\TopAdmin\Alert\Components;
+
+use TopSystem\TopAdmin\Alert;
+
+abstract class AbstractComponent implements ComponentInterface
+{
+    protected $alert;
+
+    public function setAlert(Alert $alert)
+    {
+        $this->alert = $alert;
+
+        return $this;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->alert, $name], $arguments);
+    }
+}
---
+++ src/Alert/Components/TitleComponent.php
@@ -0,0 +1,17 @@
+<?php
+namespace TopSystem\TopAdmin\Alert\Components;
+
+class TitleComponent extends AbstractComponent
+{
+    protected $title;
+
+    public function create($title)
+    {
+        $this->title = $title;
+    }
+
+    public function render()
+    {
+        return "<strong>{$this->title}</strong>";
+    }
+}
